bad3dad3abc16ca546a7d00f8e798ecc0a041f0d
ajwulf <ajwulf.13@gmail.com>
2016-02-16 12:09:20 -0800

    blerg

--- html/index.php
+++ html/index.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?php
+include('arrayinclude.php');
+
+$maincat = filter_var($_REQUEST['category'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,119 +13,66 @@
     <script href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-<h1>Applebee's Menu</h1>
-<div class="container">
-    <div class="row">
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/2for20.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">2 for $20</a>
-            </h3>
-        </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/barSnacks.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Apps and Bar Snacks</a>
-            </h3>
-        </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/betterforyou.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Better For You</a>
-            </h3>
-        </div>
-    </div>
-    <!-- /.row -->
-​
-    <!-- Projects Row -->
-    <div class="row">
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/beverages.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Get Drunk</a>
-            </h3>
-        </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/burgers.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Burgers</a>
-            </h3>
-        </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/desserts.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Desserts</a>
-            </h3>
-        </div>
-    </div>
-​
-    <!-- Projects Row -->
-    <div class="row">
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/entree_main_dishes.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Main Entrees</a>
-            </h3>
-​
-        </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/handhelds.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Handhelds</a>
-            </h3>
-        </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/kids.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Kids</a>
-            </h3>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/lunch_combos.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Combos</a>
-            </h3>
-        </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/pubdiet.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Pub Diet</a>
-            </h3>
-        </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="./img/maincat/salads.jpg" alt="">
-            </a>
-            <h3>
-                <a href="#">Salads</a>
-            </h3>
+    <h1>AppleBee's Menu</h1>
+    <div class='container'>
+        <div class='row'>
+            <div class='col-md-4 portfolio-item'>
+<?php
+switch ($maincat){
+    case "":
+        foreach($Menu as $value) {
+            echo "<a href='".$value['Link']."'>
+                <img class='img-responsive' src='".$value['ImageLoc']."' alt=''>
+                <h3>".$value['Title']."</h3>
+            </a>";
+        }
+
+        break;
+    case "twofor":
+        <!-- var_dump($Menu[0]); -->
+        $twofor = $Menu[0];
+        foreach($twofor as $value) {
+            echo $value;
+        }
+
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+    case "":
+        //code for category goes here;
+        break;
+}
+?>
         </div>
     </div>
 </div>
-<!-- /.row -->
 </body>
 </html>
