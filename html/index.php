f893038ba29d096bb62f8c0d7b75f4d65cd52e9f
ajwulf <ajwulf.13@gmail.com>
2016-02-15 17:00:07 -0800

    initial commit maybe

---
+++ html/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Applebee's Menu</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1>Applebee's Menu</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/2for20.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">2 for $20</a>
+            </h3>
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/barSnacks.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Apps and Bar Snacks</a>
+            </h3>
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/betterforyou.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Better For You</a>
+            </h3>
+        </div>
+    </div>
+    <!-- /.row -->
+​
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/beverages.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Get Drunk</a>
+            </h3>
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/burgers.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Burgers</a>
+            </h3>
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/desserts.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Desserts</a>
+            </h3>
+        </div>
+    </div>
+​
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/entree_main_dishes.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Main Entrees</a>
+            </h3>
+​
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/handhelds.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Handhelds</a>
+            </h3>
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/kids.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Kids</a>
+            </h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/lunch_combos.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Combos</a>
+            </h3>
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/pubdiet.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Pub Diet</a>
+            </h3>
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="./img/maincat/salads.jpg" alt="">
+            </a>
+            <h3>
+                <a href="#">Salads</a>
+            </h3>
+        </div>
+    </div>
+</div>
+<!-- /.row -->
+</body>
+</html>
